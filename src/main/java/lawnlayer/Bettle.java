c8c03a242008e02529ecd3ae531b249516d264f3
ZJ <zhaozj1225@outlook.com>
2022-04-30 17:27:20 +1000

    javadoc

--- src/main/java/lawnlayer/Bettle.java
+++ src/main/java/lawnlayer/Bettle.java
@@ -2,8 +2,23 @@ package lawnlayer;
 
 import processing.core.PImage;
 
+/**
+ * This class is inherited from {@code enemy} class
+ * Any bettle whose type is set to 1.
+ *
+ * @author Zijie Zhao
+ * @version 1.0
+ * @since 22/04/2022
+ */
 public class Bettle extends Enemy{
 
+    /**
+     * Class constructor.
+     *
+     * @param x the x coordinate of this bettle
+     * @param y the y coordinate of this bettle
+     * @param sprite the image coordinate of this bettle
+     */
     public Bettle(int x, int y, PImage sprite) {
         super(x, y, sprite);
         this.type = 1;
