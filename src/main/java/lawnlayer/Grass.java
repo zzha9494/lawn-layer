c183316154af5dd3e3c7afff3c50f855564f0440
ZJ <zhaozj1225@outlook.com>
2022-04-30 17:42:32 +1000

    javadoc

--- src/main/java/lawnlayer/Grass.java
+++ src/main/java/lawnlayer/Grass.java
@@ -2,8 +2,24 @@ package lawnlayer;
 
 import processing.core.PImage;
 
+/**
+ * This class is inherited from {@code Tile} class.
+ * A grass tile can be created by a player on condition
+ * and be destroyed by a bettle.
+ *
+ * @author Zijie Zhao
+ * @version 1.0
+ * @since 22/04/2022
+ */
 public class Grass extends Tile{
 
+    /**
+     * Class constructor.
+     *
+     * @param x the x coordinate of this grass
+     * @param y the y coordinate of this grass
+     * @param sprite the image of this grass
+     */
     public Grass(int x, int y, PImage sprite) {
         super(x, y, sprite);
     }
