295be46cc849c806f154923df40fa9bd2caa48b6
ZJ <zhaozj1225@outlook.com>
2022-04-30 20:20:00 +1000

    javadoc

--- src/main/java/lawnlayer/Powerup.java
+++ src/main/java/lawnlayer/Powerup.java
@@ -2,9 +2,25 @@ package lawnlayer;
 
 import processing.core.PImage;
 
+/**
+ * This class is inherited from {@code Tile} class.
+ * A powerup can randomly respawn and has a random type.
+ * The randomness is arranged when initialization.
+ *
+ * @author Zijie Zhao
+ * @version 1.0
+ * @since 22/04/2022
+ */
 public class Powerup extends Tile implements Spawn {
     int type;
 
+    /**
+     * Class constructor.
+     * Automatically call methods to allocate a random position and type.
+     * (The number 2 in {@code randomInterval % 2;} can be set as a static final attribute if you like.)
+     *
+     * @param app the running app
+     */
     public Powerup(App app) {
         super(0, 0);
         this.randomSpawn(app);
@@ -12,6 +28,12 @@ public class Powerup extends Tile implements Spawn {
         this.sprite = this.chooseType(app);
     }
 
+    /**
+     * Randomly set position and check if this position is empty.
+     * The boundary of the map has been excepted.
+     *
+     * @param app the running app
+     */
     public void randomSpawn(App app) {
         while(true) {
             int x = (int)(Math.random()*62);
@@ -25,6 +47,11 @@ public class Powerup extends Tile implements Spawn {
         }
     }
 
+    /**
+     *
+     * @param app the running app
+     * @return
+     */
     public PImage chooseType(App app) {
         if (this.type == 0)
             return app.powerup_0;
