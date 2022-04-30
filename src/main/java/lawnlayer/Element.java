d25feed750567f50e36f24fad1083807d68a92b1
ZJ <zhaozj1225@outlook.com>
2022-04-30 17:09:10 +1000

    javadoc

--- src/main/java/lawnlayer/Element.java
+++ src/main/java/lawnlayer/Element.java
@@ -22,7 +22,8 @@ enum Direction {
 }
 
 /**
- * Implement the way of spawn. Only {@code randomSpawn} offered currently.
+ * An interface that implement the way of spawn.
+ * Only {@code randomSpawn} offered currently.
  */
 interface Spawn {
     void randomSpawn(App app);
@@ -43,9 +44,9 @@ public class Element {
     /**
      * Class constructor.
      *
-     * @param x the x coordinate of this element.
-     * @param y the y coordinate of this element.
-     * @param sprite the image of this element.
+     * @param x the x coordinate of this element
+     * @param y the y coordinate of this element
+     * @param sprite the image of this element
      */
     public Element(int x, int y, PImage sprite) {
         this.x = x;
@@ -53,20 +54,46 @@ public class Element {
         this.sprite = sprite;
     }
 
+    /**
+     * Class constructor. Do not have an image first.
+     *
+     * @param x the x coordinate of this element
+     * @param y the y coordinate of this element
+     */
     public Element(int x, int y) {
         this(x, y, null);
     }
 
+    /**
+     * This method is to check whether the tile {@code t} is clinging to it.
+     *
+     * @param t the tile that you want to check
+     * @return {@code true} if it is next to the another
+     */
     public boolean checkCling(Tile t) {
         if (this.x == t.x && (this.y == t.y + 20 || this.y + 20 == t.y))
             return true;
         return this.y == t.y && (this.x == t.x + 20 || this.x + 20 == t.x);
     }
 
+    /**
+     * This method is to check whether the tile {@code t} is colliding with it.
+     *
+     * @param t the tile that you want to check
+     * @return {@code true} if they are colliding
+     */
     public boolean checkCollide(Element t) {
         return Math.abs(this.x - t.x) < 20 && Math.abs(this.y - t.y) < 20;
     }
 
+    /**
+     * Check whether a tile is colliding with at least one tile in a region.
+     * A region consists of a set of tiles. They are no need to completely continuous.
+     *
+     * @param region a region you want to check
+     * @param <T> any element
+     * @return {@code true} if a tile is colliding with
+     */
     public <T extends Element> boolean checkInRegion (ArrayList<T> region) {
         for (T t: region)
             if (this.checkCollide(t))
@@ -74,6 +101,11 @@ public class Element {
         return false;
     }
 
+    /**
+     * This method is to draw an element on the board.
+     *
+     * @param app where a board is running on.
+     */
     public void draw(PApplet app) {
         app.image(this.sprite, this.x, this.y);
     }
